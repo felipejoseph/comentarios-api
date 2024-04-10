4d0f56202acfc31de3966284e59676184fc935f1
Felipe <felipejoseph45@hotmail.com>
2024-04-10 18:21:49 -0300

    ajuste dockerhub

--- app/api.py
+++ app/api.py
@@ -1,14 +1,13 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
-
+from flask import Flask, jsonify, request
+from flask_swagger_ui import get_swaggerui_blueprint
+import yaml
 
 app_name = 'comentarios'
 app = Flask(app_name)
 
-
 comments = {}
 
+# Definição das rotas da API
 @app.route('/api/comment/new', methods=['POST'])
 def api_comment_new():
     request_data = request.get_json()
@@ -34,7 +33,6 @@ def api_comment_new():
             }
     return jsonify(response)
 
-
 @app.route('/api/comment/list/<content_id>')
 def api_comment_list(content_id):
     content_id = '{}'.format(content_id)
@@ -48,6 +46,25 @@ def api_comment_list(content_id):
                 'message': message,
                 }
         return jsonify(response), 404
+
+# Servindo a especificação Swagger
+@app.route('/swagger/spec')
+def spec():
+    # Carregar o arquivo Swagger
+    with open('swagger.yaml', 'r') as f:
+        swagger_spec = yaml.safe_load(f)
+    return jsonify(swagger_spec)
+
+# Configurando a interface Swagger
+SWAGGER_URL = '/api/docs'
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    '/swagger/spec',
+    config={
+        'app_name': "API de Comentários"
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
